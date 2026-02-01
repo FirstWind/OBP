4b99715eb1427dce159f501bf6fbcfca51b43fc3
firstwind <firstwind@mail.ru>
2026-02-01 17:27:41 +0900

    Add exercise grade integration test

---
+++ src/tests/TestExerciseGradeIntegration.pas
@@ -0,0 +1,49 @@
+program TestExerciseGradeIntegration;
+
+{$mode objfpc}{$H+}
+
+uses
+  SysUtils, Scales, ScaleScoreService, ExerciseGrades, Policies;
+
+procedure AssertEqStr(const A, B, Msg: string);
+begin
+  if A <> B then
+    raise Exception.Create(Msg + ' expected=' + B + ' actual=' + A);
+end;
+
+procedure TestIntegration;
+var
+  Scs: TScales;
+  Idx: Integer;
+  Rounding: TRoundingPolicy;
+  Points: Integer;
+  Grades: TPointsGradeRows;
+  Grade: string;
+begin
+  Scs := LoadScalesFromAppendix12('src\\tests\\fixtures\\valid\\appendix12.json');
+  Idx := FindScale(Scs, 1, 'M', 1, 'base');
+  if Idx < 0 then
+    raise Exception.Create('scale not found');
+
+  Rounding.TimeSec := 'round_half_up_to_int';
+  Rounding.DistanceM := 'round_half_up_to_1';
+  Rounding.Reps := 'integer_only';
+
+  Points := CalcPointsForResult(Scs[Idx], 25, Rounding, osp_zero_points);
+  Grades := LoadExerciseGradesFromAppendix13('src\\tests\\fixtures\\valid\\appendix13.json');
+  Grade := GradeByPoints(Grades, Points);
+  AssertEqStr(Grade, 'satisfactory', 'exercise grade');
+end;
+
+begin
+  try
+    TestIntegration;
+    WriteLn('OK');
+  except
+    on E: Exception do
+    begin
+      WriteLn('FAIL: ' + E.Message);
+      Halt(1);
+    end;
+  end;
+end.
